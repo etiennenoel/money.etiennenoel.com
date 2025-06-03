02725a62b5ca3f0ccce14ba1f3d43aa27939cd84
Etienne Noel <etiennenoel@gmail.com>
2025-06-02 20:35:15 -0700

    - Updates to the design.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,9 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {Component, Inject, OnInit} from '@angular/core';
 import {BaseComponent} from '../base/base.component';
 import {ToastStore} from '../../stores/toast.store';
 import {DOCUMENT} from '@angular/common';
-import {ToastMessageInterface} from '../../interfaces/toast-message.interface';
-import {delay, pipe} from 'rxjs';
+import {RouteEnum} from '../../enums/route.enum';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'sidebar',
@@ -14,14 +13,41 @@ import {delay, pipe} from 'rxjs';
 })
 export class SidebarComponent extends BaseComponent implements OnInit {
 
+  currentRoute: RouteEnum = RouteEnum.Home;
+
   constructor(
     private readonly toastStore: ToastStore,
     @Inject(DOCUMENT) document: Document,
+    private readonly router: Router,
   ) {
     super(document);
   }
 
   override ngOnInit() {
     super.ngOnInit();
+
+    this.subscriptions.push(this.router.events.subscribe(value => {
+      if (value && value.constructor.name === 'NavigationEnd') {
+        this.updateRoute();
+      }
+    }))
+
+    this.updateRoute();
   }
+
+  updateRoute() {
+    if(!this.window) {
+      return;
+    }
+
+    const url = this.window.location.pathname;
+
+    if(url === "") {
+      this.currentRoute = RouteEnum.Home;
+    } else {
+      this.currentRoute = url as RouteEnum;
+    }
+  }
+
+  protected readonly RouteEnum = RouteEnum;
 }
