7361d2ba1233bacc00a36d484a8d3fa19a4ecbb1
Etienne Noel <etiennenoel@gmail.com>
2025-06-14 21:23:23 -0700

    - improvements.

--- src/app/providers/labels.provider.ts
+++ src/app/providers/labels.provider.ts
@@ -10,17 +10,27 @@ let a = 1;
 
 @Injectable()
 export class LabelsProvider implements ItemsProviderInterface {
-    async getItems(query?: string): Promise<ItemInterface[]> {
-        return items.filter(items => items.label.includes(query ?? ""));
-    }
+  async getItems(query?: string): Promise<ItemInterface[]> {
+    return items.filter(items => items.label.includes(query ?? ""));
+  }
 
-  async createFrom(query?: string): Promise<void> {
-      console.log("Create From");
-      a++;
+  async createFrom(query?: string): Promise<ItemInterface> {
+    console.log("Create From");
+    a++;
 
-      items.push({
-        id: a+"",
-        label: query+"_",
-      })
+   return new Promise(resolve => {
+     let item = {
+       id: a + "",
+       label: query + "_",
+     }
+
+     items.push(item)
+
+     setTimeout(() => {
+       resolve(item);
+     }, 1000)
+   })
+
+
   }
 }
